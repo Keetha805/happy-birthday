0d3fc5ba797aa66be5883ca3b0f6c8380592f4a7
Ketha805 <isabellamauriciofraiz@gmail.com>
2023-03-07 21:12:35 +0100

    Colors and ico updated

--- src/components/ValePor.js
+++ src/components/ValePor.js
@@ -1,29 +1,31 @@
 import React from 'react'
+import convert from 'color-convert/conversions';
 
 export const ValePor = ({ regalo, numero, codigo }) => {
 
-    const hslToHex = (h, s, l) => {
-        l /= 100;
-        const a = s * Math.min(l, 1 - l) / 100;
-        const f = n => {
-            const k = (n + h / 30) % 12;
-            const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
-            return Math.round(255 * color).toString(16).padStart(2, '0');   // convert to Hex and prefix "0" if needed
-        };
-        return `#${f(0)}${f(8)}${f(4)}`;
-    }
+    // const hslToHex = (h, s, l) => {
+    //     l /= 100;
+    //     const a = s * Math.min(l, 1 - l) / 100;
+    //     const f = n => {
+    //         const k = (n + h / 30) % 12;
+    //         const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+    //         return Math.round(255 * color).toString(16).padStart(2, '0');   // convert to Hex and prefix "0" if needed
+    //     };
+    //     return `#${f(0)}${f(8)}${f(4)}`;
+    // }
 
     const generarColor = () => {
-        let h = Math.floor(Math.random() * 360)
-        let s = 84
-        let l = 90
+        let h = Math.floor(Math.random() * 20 + 30)
+        let s = 70
+        let l = 80
 
-        return hslToHex(h, s, l)
+        let rgb = convert.hsl.rgb([h, s, l])
+        let hex = `#${convert.rgb.hex(rgb)}`
+        return hex
 
     }
 
     const a = regalo == ""
-    const b = regalo == "Felicidades Tienes Todos Tus Regalos"
 
     return (
         <div style={a ? { backgroundColor: "transparent" } : { backgroundColor: generarColor() }} className={"vale"}>
