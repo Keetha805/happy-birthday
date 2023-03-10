6ee442d35c4df289458b8b98b20bfc051231a6cb
Ketha805 <isabellamauriciofraiz@gmail.com>
2023-03-10 11:41:43 +0100

    Updated imgs

--- src/components/Terminado.js
+++ src/components/Terminado.js
@@ -8,7 +8,7 @@ export const Terminado = ({ edad }) => {
             <img src={congrats} alt="Felicidades!" className="img2" />
             <p>Diploma Oficial por: <br /> Conseguir todos los vales</p>
             <p>Disfrute de su cumpleaños, y disfrute de sus <span className="numero">{edad}</span></p>
-            <p style={{ fontSize: "15px" }}>Firmado: Isabella</p>
+            <p style={{ fontSize: "15px" }}>Firmado: Keetha</p>
             <br></br>
         </div >
     )
